c8426b10d1583ea5bb59c58505fe74b8f3b7f575
elisawheaton <elisa.wheaton411@gmail.com>
2023-06-06 15:00:31 -0400

    Add project files.

---
+++ SMSAlertAPI/AlertController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json; 
+
+namespace SMSAlertAPI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlertController : ControllerBase
+    {
+        public SMTPSender sender; 
+        private string _jsonString; 
+
+        public AlertController(IConfiguration configuration)
+        {
+            sender = new SMTPSender(); 
+            _jsonString = ""; 
+        }
+
+        [HttpGet("SendSMS/{recipient}/{subject}/{body}")]
+        public async Task<IActionResult> SendSMS(string recipient, string subject, string body)
+        {
+            try
+            {   
+                await sender.SendMessage(recipient, subject, body);
+                _jsonString = JsonConvert.SerializeObject( "To: " + recipient + ": " + subject + " - " + body);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+
+            return Ok(_jsonString);
+        }
+
+    }
+}
---
+++ SMSAlertAPI/SMTPSender.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Template;
+using System.Net;
+using System.Net.Mail;
+using Newtonsoft.Json; 
+
+namespace SMSAlertAPI
+{
+    public class SMTPSender
+    {
+        private readonly string server;
+        private readonly int port;
+        private readonly string username;
+        private readonly string password;
+        private readonly SmtpClient client; 
+
+        public SMTPSender() 
+        {
+            server = "smtp.gmail.com";
+            port = 587;
+            username = "thetoolapp@gmail.com";
+            password = "ocxjxfxnpvviakuj";
+
+            client = new SmtpClient(server, port);
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(username, password);
+            client.EnableSsl = true;
+        }
+
+        public async Task SendMessage(string recipient, string subject, string body) 
+        {
+            // Create message 
+            MailMessage message = new MailMessage(username, recipient);
+            message.Subject = subject;
+            message.Body = body;
+
+            // Send the email
+            await client.SendMailAsync(message);
+
+            return; 
+        }
+
+
+        //AT&T: <phoneNumber>@txt.att.net
+        //Verizon: <phoneNumber>@vtext.com
+        //T-Mobile: <phoneNumber>@tmomail.net
+        //Sprint: <phoneNumber>@messaging.sprintpcs.com
+    }
+}
